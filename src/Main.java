ff29a0d32b5d0249e56e2d8b98aface7f06afc43
GUSTAVO ZUDREWER AGNES <12105755@pucrs.br>
2019-04-02 20:19:08 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner k = new Scanner(System.in);
+        System.out.println("Modo de operação: \n1- Servidor\n2-Cliente P2P");
+        int o = k.nextInt();
+    }
+}
